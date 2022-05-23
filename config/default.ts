b4aaa630205f001f2587dfa70d100f2153498aea
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-05-23 15:50:07 +0200

    db connection + set up

---
+++ config/default.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+
+const key = process.env.DB_KEY;
+const user = process.env.DB_USER;
+
+
+export default {
+    port: 1337,
+    dbUri: `mongodb+srv://${user}:${key}@izabela.41hbr.mongodb.net/?retryWrites=true&w=majority`
+}
